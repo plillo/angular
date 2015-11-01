a67e39b70fe9235a3ee002731dc99ca758a5247e
Paolo Lillo <paolo.lillo@hash.it>
2015-11-01 10:25:04 +0100

    test

--- it.hash.osgi.angular.test.application/static/angularJSTest/js/directives6.js
+++ it.hash.osgi.angular.test.application/static/angularJSTest/js/directives6.js
@@ -19,11 +19,25 @@ angular.module('plunker').directive('btSmartbutton', function(broker) {
 			// getTopicStatus
 			$scope.getTopicStatus = function(message){
 				// Get body message from topic
-				var payload = JSON.parse(message.body);
+				
+				var payload = undefined;
+				try {
+					var payload = JSON.parse(unescape(message));
+				}
+				catch(error) {
+					alert(error);
+					return;
+				}
 				
 				if(payload.status > $scope.statesNumber)
 					return;
 				
+				if(payload.label!=undefined){
+					var number = Number(payload.label).toFixed(2);
+
+					$scope.label = ''+number;
+				}
+					
 				$scope.status = payload.status;
 				$scope.leftBackgroundPos = -($scope.status-1) * $scope.width;
 
@@ -67,7 +81,10 @@ angular.module('plunker').directive('btSmartbutton', function(broker) {
 				if($scope.topic) {
 					broker.deferred.then(
 						function(message){
-							broker.client.send($scope.topic, {retain:'set'}, JSON.stringify({status:$scope.status}));
+							var sendmessage = new Paho.MQTT.Message(JSON.stringify({status:$scope.status}));
+							sendmessage.destinationName = $scope.topic;
+							sendmessage.retained = true;
+							broker.client.send(sendmessage);
 						},
 						function(message){
 							alert('Promise rejected with message: '+message);
@@ -78,6 +95,9 @@ angular.module('plunker').directive('btSmartbutton', function(broker) {
 		link: function(scope, element, attributes){
 			// Set <element> in scope
 			scope.element = element.find('.bt-smartbutton');
+			
+		    if(scope.label==undefined)
+		    	scope.element.find('.bt-label>span').hide();
 
 			// Get from attribute 'status' and Set <status> in scope
 			scope.status = parseInt(attributes['status']);
@@ -86,7 +106,7 @@ angular.module('plunker').directive('btSmartbutton', function(broker) {
 			if(scope.topic) {
 				broker.deferred.then(
 					function(message){
-						broker.client.subscribe(scope.topic, scope.getTopicStatus);
+						broker.subscribe(scope.topic, scope.getTopicStatus);
 					},
 					function(message){
 						alert('Promise rejected with message: '+message);
@@ -95,8 +115,12 @@ angular.module('plunker').directive('btSmartbutton', function(broker) {
 
 			scope.width = parseInt(attributes['width']);
    			scope.height = parseInt(attributes['height']);
+			scope.iconImg = attributes['icon'];
 			scope.backgroundImg = attributes['background'];
 			scope.leftBackgroundPos = -(scope.status-1) * scope.width;
+			
+			var v_unit = scope.height/20;
+			var h_unit = scope.width/20;
 
 			// Get from attribute 'states-number' Set <statesNumber> in scope
 			scope.statesNumber = parseInt(attributes['statesnumber']);
@@ -108,12 +132,35 @@ angular.module('plunker').directive('btSmartbutton', function(broker) {
 			scope.element.css('width', scope.width);
    			scope.element.css('height', scope.height);
 
+   			// -- icon image
+   			if(scope.iconImg!==undefined){
+   				var img = $('<img>');
+   				img.attr('src', scope.iconImg);
+   				img.css('width', (v_unit*10)+'px');
+   				img.css('display', 'inline-block');
+   				img.css('margin', (v_unit*5)+'px 0 0 '+(v_unit*5)+'px');
+   				img.appendTo(scope.element.find('.bt-icon'));
+   			}
+   			else {
+   				scope.element.find('.bt-icon').hide();
+   			}
+   			
+			scope.element.find('.bt-label').css('position', 'absolute');
+			scope.element.find('.bt-label').css('top', (scope.height-h_unit*4)+'px');
+   			
+			var w = scope.element.find('.bt-label>span').width();
+
+			scope.element.find('.bt-label>span').css('display', 'table');
+			scope.element.find('.bt-label>span').css('width', (h_unit*18)+'px');
+			scope.element.find('.bt-label>span').css('font-size', (h_unit*2)+'px');
+   			scope.element.find('.bt-label>span').css('margin', '0 '+h_unit+'px');
+   			
    			// -- background image
    			if(scope.backgroundImg!==undefined)
    				scope.element.css('background-image','url('+scope.backgroundImg+')');
    			scope.element.css('background-position',scope.leftBackgroundPos+'px -'+scope.height*2+'px');
    			// -- line-height
-   			scope.element.find('.bt-label').css('line-height', scope.height+'px');
+   			//scope.element.find('.bt-label').css('line-height', scope.height+'px');
 
    			// EVENTS BINDING
    			scope.element.bind('mouseover', scope.onMouseOver);
@@ -124,35 +171,39 @@ angular.module('plunker').directive('btSmartbutton', function(broker) {
 	};
 });
 
-angular.module('plunker').directive('myTag', function() {
-return {
-  scope: {},
-  template: 'WOW {{name}}!<scan ng-click="sclick()">click</scan>-<scan ng-click="kclick()">click</scan><br /><input ng-click="click()" name="name" ng-model="name" /><br />{{status}}.{{clicked}}.<span id="iamk"></span>',
-  controller: function($scope, $element){
-       $scope.clicked = 0;
-       var k =0;
-       $scope.click = function(){
-         $scope.clicked++;
-       };
-       $scope.sclick = function(){
-         $scope.status++;
-       };
-       $scope.kclick = function(){
-         k++;
-         $element.find('#iamk').html(k);
-       };
-    },
-  compile: function(element, attributes){
-    // qui si possono fare le trasformazioni del DOM che vengono eseguite prima della LINK function...
-		element.css("color","red");
+angular.module('plunker').directive('btMulti', function() {
+	return {
+		replace: false,
+		scope: {
+			number:'@'
+		},
+		templateUrl : 'template/bt-multi.html',
+		controller: function($scope, $element){
+	       	// insert here scope-properties
+			// ...
 
-		// ...poi si restituisce la LINK function
-		return function(scope, element, attributes){
-		  scope.name = 'pippo';
-		  scope.status=parseInt(attributes['status']);
-		  
-		  console.log(status);
-    }
-  }
-}
+	       	// insert here scope-functions
+			// ...
+			
+	    },
+        compile: function(element, attributes){
+        	// Do here DOM transformations which are executed before than LINK function...
+    		// ...
+        	var number = Number(attributes['number']);
+        	var inner = element.find('bt-multi').html();
+
+        	if(number)
+	    		for(var i=0; i<number; i++){
+	    			if(i%20==0)
+	    				element.find('.bt-multi').append('<br />');
+	    			element.find('.bt-multi').append('<bt-smartbutton label="'+i+'" width="50" height="50" status="1" statesnumber="3"></bt-smartbutton>');
+	    		}
+
+    		// ...then RETURN the link function
+    		return function(scope, element, attributes){
+    			// ...
+
+        }
+      }
+	};
 });
